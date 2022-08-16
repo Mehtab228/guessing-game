b53eea3acf0dc56c9b78a483e9e7a6b4bc36964d
Mehtab Riar <mehtabriar228@hotmail.com>
2022-08-16 15:10:31 -0700

    Added some more questions and responses using switch

--- app.js
+++ app.js
@@ -12,18 +12,68 @@ function getName(){
 
 getName();
 
-let answer = prompt('Do I like the outdoors? Please answer yes or no');
-if (typeof(answer) === 'string'){
-  answer = answer.toLowerCase();
+let outdoors = prompt('Do I like the outdoors? Please answer yes or no');
+if (typeof(outdoors) === 'string'){
+  outdoors = outdoors.toLowerCase();
 }
 
-switch (answer){
-case 'yes || y':
+switch (outdoors){
+case 'yes' || 'y':
   alert('You\'re correct!');
   break;
 case 'no || n':
   alert('Ah, try again next time!');
   break;
+default: 
+  alert('Try again next time!');
+}
+
+let pollen = prompt('Is pollen the worst? Please answer yes or no!');
+if (typeof(pollen) === 'string'){
+  pollen = pollen.toLowerCase();
+}
+
+switch (pollen){
+case 'yes' || 'y':
+  alert('You\'re correct!');
+  break;
+case 'no || n':
+  alert('Come on it\'s literally the worst!');
+  break;
+default:
+  alert('Try again next time!');
+}
+
+let sushi = prompt('Do I like sushi? Please answer yes or no!');
+if (typeof(sushi) === 'string'){
+  sushi = sushi.toLocaleLowerCase();
+}
+
+switch (sushi){
+case 'yes' || 'y':
+  alert('You are correct!');
+  break;
+case 'no' || 'n':
+  alert('Try again next time!');
+  break;
+default:
+  alert('You don\'t like following instructions do you?');
+}
+
+let newgirl = prompt('Is New Girl one of my favourite shows? Please answer yes or no!');
+if (typeof(newgirl) === 'string'){
+  newgirl = newgirl.toLocaleLowerCase();
+}
+
+switch (newgirl){
+case 'yes' || 'y':
+  alert('You\'re right and you have great taste in tv shows!');
+  break;
+case 'no' || 'n':
+  alert('You need to watch the show!');
+  break;
+default:
+  alert('Come on thats the 4th time! Try again!');
 }
 
 
