d6b3cf454a23657e5cc255dbabbccc824fe89a55
Mehtab Riar <mehtabriar228@hotmail.com>
2022-08-17 21:36:12 -0700

    changed names of arrays

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-/*
+
 function getName() {
   let user = prompt('What is your name?');
   if (user === '' || user === null || undefined) {
@@ -12,22 +12,23 @@ function getName() {
 
 getName();
 
+
 let outdoors = prompt('Do I like the outdoors? Please answer yes or no');
 if (typeof (outdoors) === 'string') {
   outdoors = outdoors.toLowerCase();
 }
 
 switch (outdoors) {
-  case 'y':
-  case 'yes':
-    alert('You\'re correct!');
-    break;
-  case 'no':
-  case 'n':
-    alert('Incorrect Answer!');
-    break;
-  default:
-    alert('Try again next time!');
+case 'y':
+case 'yes':
+  alert('You\'re correct!');
+  break;
+case 'no':
+case 'n':
+  alert('Incorrect Answer!');
+  break;
+default:
+  alert('Try again next time!');
 }
 
 let pollen = prompt('Is pollen the worst? Please answer yes or no!');
@@ -36,16 +37,16 @@ if (typeof (pollen) === 'string') {
 }
 
 switch (pollen) {
-  case 'yes':
-  case 'y':
-    alert('You\'re correct!');
-    break;
-  case 'no':
-  case 'n':
-    alert('Come on it\'s literally the worst!');
-    break;
-  default:
-    alert('Try again next time!');
+case 'yes':
+case 'y':
+  alert('You\'re correct!');
+  break;
+case 'no':
+case 'n':
+  alert('Come on it\'s literally the worst!');
+  break;
+default:
+  alert('Try again next time!');
 }
 
 let sushi = prompt('Do I like sushi? Please answer yes or no!');
@@ -54,16 +55,16 @@ if (typeof (sushi) === 'string') {
 }
 
 switch (sushi) {
-  case 'yes':
-  case 'y':
-    alert('You are correct!');
-    break;
-  case 'no':
-  case 'n':
-    alert('Incorrect Answer!');
-    break;
-  default:
-    alert('You don\'t like following instructions do you?');
+case 'yes':
+case 'y':
+  alert('You are correct!');
+  break;
+case 'no':
+case 'n':
+  alert('Incorrect Answer!');
+  break;
+default:
+  alert('You don\'t like following instructions do you?');
 }
 
 let newgirl = prompt('Is New Girl one of my favourite shows? Please answer yes or no!');
@@ -72,16 +73,16 @@ if (typeof (newgirl) === 'string') {
 }
 
 switch (newgirl) {
-  case 'yes':
-  case 'y':
-    alert('You\'re right and you have great taste in tv shows!');
-    break;
-  case 'no':
-  case 'n':
-    alert('You need to watch the show!');
-    break;
-  default:
-    alert('Come on thats the 4th time! Try again!');
+case 'yes':
+case 'y':
+  alert('You\'re right and you have great taste in tv shows!');
+  break;
+case 'no':
+case 'n':
+  alert('You need to watch the show!');
+  break;
+default:
+  alert('Come on thats the 4th time! Try again!');
 }
 
 let basketball = prompt('Do I like to watch basketball? Please answer yes or no!')
@@ -90,16 +91,16 @@ if (typeof (basketball) === 'string') {
 }
 
 switch (basketball) {
-  case 'yes':
-  case 'y':
-    alert('You\'re right');
-    break;
-  case 'no':
-  case 'n':
-    alert('Incorrect Answer!');
-    break;
-  default:
-    alert('Try again next time!');
+case 'yes':
+case 'y':
+  alert('You\'re right');
+  break;
+case 'no':
+case 'n':
+  alert('Incorrect Answer!');
+  break;
+default:
+  alert('Try again next time!');
 }
 
 let questions = [
@@ -116,47 +117,49 @@ for (let i = 0; i < 4; i++) {
     alert('You are correct!');
     break;
   }
-  else if (response === '1' || response === '2'){
+  else if (response === '1' || response === '2') {
     alert('Guess is way too low');
   }
-  else if (response === '3'){
+  else if (response === '3') {
     alert('you\'re so close!');
   }
 }
 
-*/
 
-let questions = [
+
+let newquestions = [
   'What is my favourite movie?'
 ];
 
-let answers = [
+let newanswers = [
   'The Other Guys',
   'Hot Fuzz',
   'Harry Potter and the Prisoner of Azkaban'
 ];
+
 let guess = false;
 for (let i = 0; i < 6; i++) {
   console.log('I loop ', i);
-  if (guess === true){
+  if (guess === true) {
     console.log('Guess is true, break i loop');
     break;
   }
-  let response = prompt(questions[0]);
+  let response = prompt(newquestions[0]);
   response = response.toLowerCase();
-  for (let j = 0; j < answers.length; j++){
+  for (let j = 0; j < newanswers.length; j++) {
     console.log('J loop ', j);
-    if (response === answers[j].toLowerCase()) {
+    if (response === newanswers[j].toLowerCase()) {
       alert('You are correct!');
       guess = true;
       break;
     }
     else {
-      console.log(answers[j] + ' is wrong');
+      console.log(newanswers[j] + ' is wrong');
     }
 
-  } if (guess === false){
-    alert('Hint:Think of Wizards!');
+  } if (guess === false) {
+    alert('Hint: Think of Wizards and a prisoner!');
   }
 }
+alert('Correct answers were The Other Guys, Hot Fuzz, and Harry Potter and the Prisoner of Azkaban');
 
