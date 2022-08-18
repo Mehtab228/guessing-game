f9f33d269ae60a74ff8a6c5e4c1522865236b10a
Cianenry Danan <cianenryd@gmail.com>
2022-08-18 13:16:50 -0700

    Created functions.

--- app.js
+++ app.js
@@ -2,56 +2,65 @@
 
 let score = 0;
 
-function getName() {
-  let user = prompt('What is your name?');
-  if (user === '' || user === null || undefined) {
-    alert('That\'s not a name try again!');
-    getName();
-  } else {
-    alert('Hey ' + user + ' welcome to my page! ');
+// function getName() {
+//   let user = prompt('What is your name?');
+//   if (user === '' || user === null || undefined) {
+//     alert('That\'s not a name try again!');
+//     getName();
+//   } else {
+//     alert('Hey ' + user + ' welcome to my page! ');
+//   }
+// }
+
+// getName();
+
+// function questionOne() {
+//   let outdoors = prompt('Do I like the outdoors? Please answer yes or no');
+//   while (outdoors === null || outdoors === '') {
+//     alert('Please follow the instuctions you potato.');
+//     outdoors = prompt('Do I like the outdoors? Please answer yes or no');
+//   }
+//   outdoors = outdoors.toLowerCase();
+//   switch (outdoors) {
+//   case 'y':
+//   case 'yes':
+//     alert('You\'re correct!');
+//     score++;
+//     break;
+//   case 'no':
+//   case 'n':
+//     alert('Incorrect Answer!');
+//     break;
+//   default:
+//     alert('Try again next time!');
+//   }
+// }
+
+// questionOne();
+
+function questionTwo() {
+  let pollen = prompt('Is pollen the worst? Please answer yes or no!');
+  while (pollen === null || pollen === '') {
+    alert('Please follow the instuctions you potato.');
+    pollen = prompt('Do I like the outdoors? Please answer yes or no');
+  }
+  pollen = pollen.toLowerCase();
+  switch (pollen) {
+    case 'yes':
+    case 'y':
+      alert('You\'re correct!');
+      score++;
+      break;
+    case 'no':
+    case 'n':
+      alert('Come on it\'s literally the worst!');
+      break;
+    default:
+      alert('Try again next time!');
   }
 }
+// questionTwo();
 
-getName();
-
-
-let outdoors = prompt('Do I like the outdoors? Please answer yes or no');
-if (typeof (outdoors) === 'string') {
-  outdoors = outdoors.toLowerCase();
-}
-
-switch (outdoors) {
-case 'y':
-case 'yes':
-  alert('You\'re correct!');
-  score++;
-  break;
-case 'no':
-case 'n':
-  alert('Incorrect Answer!');
-  break;
-default:
-  alert('Try again next time!');
-}
-
-let pollen = prompt('Is pollen the worst? Please answer yes or no!');
-if (typeof (pollen) === 'string') {
-  pollen = pollen.toLowerCase();
-}
-
-switch (pollen) {
-case 'yes':
-case 'y':
-  alert('You\'re correct!');
-  score++
-  break;
-case 'no':
-case 'n':
-  alert('Come on it\'s literally the worst!');
-  break;
-default:
-  alert('Try again next time!');
-}
 
 let sushi = prompt('Do I like sushi? Please answer yes or no!');
 if (typeof (sushi) === 'string') {
